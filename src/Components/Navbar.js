6d0f4ff8c37dab273ca4d88c5f94be2c12531ac2
Freedy-R <99390897+Freedy-R@users.noreply.github.com>
2023-09-24 13:11:07 +0200

    change navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,17 +6,25 @@ import fireLogo from "./icons/logo.png";
 import BurgerMenuButton from "./logic/burger-menu";
 const Navbar = () => {
   return (
-    <section className="Navbar">
-      <section className="logo_section " aria-hidden="true">
-        <BurgerMenuButton></BurgerMenuButton>
-        <img src={fireLogo} alt=""></img>
-        <h1 className="logo_main">JW - Just Watch It</h1>
+    <section className="Navbar_section">
+      <section className="Navbar">
+        <section className="logo_section " aria-hidden="true">
+          <BurgerMenuButton></BurgerMenuButton>
+          <img src={fireLogo} alt=""></img>
+          <h1 className="logo_main">JW - Just Watch It</h1>
+        </section>
+        <section className="Navbar_right_section">
+          <img src={search} id="search_button" alt="wyszukaj"></img>
+          <div className="search_frame hidden">
+            <input type="search" className="Search_bar" />
+          </div>
+          <img
+            src={menu_dotted}
+            id="settings_button"
+            alt="ustawienia konta"
+          ></img>
+        </section>
       </section>
-      <img src={search} id="search_button" alt="wyszukaj"></img>
-      <div className="search_frame hidden">
-        <input type="search" className="Search_bar" />
-      </div>
-      <img src={menu_dotted} id="settings_button" alt="ustawienia konta"></img>
     </section>
   );
 };
