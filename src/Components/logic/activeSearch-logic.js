4d96a71d65707e71e76c6b96e3850749c5a5083a
Freedy-R <99390897+Freedy-R@users.noreply.github.com>
2023-10-05 21:00:21 +0200

    Search fix

--- src/Components/logic/activeSearch-logic.js
+++ src/Components/logic/activeSearch-logic.js
@@ -1,10 +1,60 @@
+import React, { useState, useEffect, useRef } from "react";
 import search from "../icons/search.png";
+
 const ActiveSearch = () => {
-    return (
-        <section className="searchSection">
-            <img src={search}></img>
-            <input type="text" placeholder="Szukaj"></input>
-        </section>
-    )
-}
-export default ActiveSearch
+  const [visible, setVisible] = useState(true);
+  const [inputValue, setInputValue] = useState("");
+  const inputRef = useRef(null);
+  const hideTimeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (inputValue === "") {
+      hideTimeoutRef.current = setTimeout(() => {
+        setVisible(false);
+      }, 3000);
+    }
+    return () => clearTimeout(hideTimeoutRef.current);
+  }, [inputValue]);
+
+  const handleMouseEnter = () => {
+    if (!visible) {
+      setVisible(true);
+      clearTimeout(hideTimeoutRef.current);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const newValue = e.target.value;
+    setInputValue(newValue);
+    if (newValue !== "") {
+      clearTimeout(hideTimeoutRef.current);
+    } else if (newValue === "" && visible) {
+      hideTimeoutRef.current = setTimeout(() => {
+        setVisible(false);
+      }, 3000);
+    }
+  };
+
+  const handleBlur = () => {
+    setVisible(false);
+  };
+
+  return visible ? (
+    <section className="searchSection" onMouseEnter={handleMouseEnter}>
+      <img src={search} alt="search" />
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Szukaj"
+        onFocus={() => {
+          clearTimeout(hideTimeoutRef.current);
+        }}
+        onBlur={handleBlur}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+    </section>
+  ) : null;
+};
+
+export default ActiveSearch;
--- src/Components/logic/search-logic.js
+++ src/Components/logic/search-logic.js
@@ -1,6 +1,6 @@
 import React, { createElement, useState } from "react";
 import search from "../icons/search.png";
-import ActiveSearch from "./activeSearch-logic";
+import ActiveSearch from "./activeSearch-logic"; // Poprawny import
 
 const Search = () => {
   const [showSearch, setShowSearch] = useState(false);
@@ -11,12 +11,12 @@ const Search = () => {
 
   return (
     <>
-    {showSearch && <ActiveSearch/>}
+      {showSearch && <ActiveSearch />} {/* Wyświetl ActiveSearch, jeśli showSearch jest true */}
       <button onClick={ToggleSearch} id="search_button">
         <img src={search} alt="wyszukaj" />
-      </button>  
+      </button>
     </>
   );
-}
+};
 
 export default Search;
