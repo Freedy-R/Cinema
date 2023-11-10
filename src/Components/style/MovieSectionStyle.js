aae44f6156abdd88178275a98f6c0ca6b450d646
Freedy-R <99390897+Freedy-R@users.noreply.github.com>
2023-11-10 14:43:39 +0100

    fix links to do navbar

--- src/Components/style/MovieSectionStyle.js
+++ src/Components/style/MovieSectionStyle.js
@@ -14,7 +14,7 @@ export const MoviesBanner = styled.section`
 export const MoviesSectionPhotoStyle = styled.section`
   position: relative;
   display: grid;
-  z-index: -1;
+  z-index: 1;
   grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   grid-template-rows: auto 1f;
   gap: 0.5rem;
@@ -31,7 +31,6 @@ export const MoviesSectionPhotoStyle = styled.section`
 `;
 export const moviePhoto = styled.div`
   display: grid;
-  z-index: -1;
   position: relative;
   grid-auto-rows: auto 1fr;
   text-align: center;
